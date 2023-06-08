f320489535bd8da292249c4ef23d69494653a29e
shashi_kumar <shashi.kumar@gingerwebs.co.in>
2023-06-08 17:33:13 +0530

    Add routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { ProductsComponent } from './products.component';
 import { ProductComponent } from './product/product.component';
 import { FormsModule } from '@angular/forms';
 import { productsService } from './products.service';
+import { HomeComponent } from './home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [productsService],
   bootstrap: [AppComponent]
